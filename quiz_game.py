e0d4c08a4ae29e398886f22bd096601cc8ff929b
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2022-11-30 14:50:15 -0800

    created a responce

--- quiz_game.py
+++ quiz_game.py
@@ -4,7 +4,8 @@ print("Welcome to my computer quiz game!")
 #varaiable that uses the input funtion 
 playing = input("Do you want to play? ")
 
-
 #if statement answering the playing variable with a quit function
 if playing.lower() != "yes":
     quit()
+
+print("Awesome! Let's do this")  
