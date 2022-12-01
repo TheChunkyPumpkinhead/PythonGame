983a4017ff487a1b0acadaaf1cdb269cc2e21adc
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2022-11-30 19:58:57 -0800

    added variable score and iterations

--- quiz_game.py
+++ quiz_game.py
@@ -8,37 +8,45 @@ playing = input("Do you want to play? ")
 if playing.lower() != "yes":
     quit()
 
-print("Awesome! Let's do this")  
+print("Awesome! Let's do this") 
+
+#variable to keep score
+score = 0 
 
 
 #variable to get input from user and an if statement
 answer = input("Are you using a computer? ")
 if answer.lower() == "yes":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect")
 
 answer = input("Is the sky up? ")
 if answer.lower() == "yes":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect")
 
 answer = input("Do cats meow? ")
 if answer.lower() == "yes":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect")
 
 answer = input("Do dogs bark? ")
 if answer.lower() == "yes":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect")
 
 answer = input("Do turtles swim? ")
 if answer.lower() == "yes":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect")
 
